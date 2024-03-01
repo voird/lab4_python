ffc530f9898cbf66093666ffc0f7d296890c48e0
polina_storozh <58592174+voird@users.noreply.github.com>
2024-03-01 11:53:28 +0300

    Update 2_program.py

--- 2_program.py
+++ 2_program.py
@@ -1,1 +1,8 @@
+import re
+import numpy as np
 
+
+filename = "D:/лабораторные работы по питону/4/polynomial.txt"
+a, b = 0, 10  # Задаем нужный отрезок [a, b]
+degree, coefficients = read_polynomial(filename)
+write_table_values(filename, a, b, degree, coefficients)
